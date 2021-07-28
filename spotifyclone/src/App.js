492f648347a0f10f96010ca0ef8c64e66a5fee97
mcrosey <80073115+mcrosey@users.noreply.github.com>
2021-07-28 19:29:03 -0400

    basic layout and styling

--- spotifyclone/src/App.js
+++ spotifyclone/src/App.js
@@ -4,11 +4,12 @@ import Login from './Login';
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './Player'
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -16,25 +17,34 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
 
       spotify.setAccessToken(_token);
 
-      spotify.getMe().then(user => {
-        console.log('person', user);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
       });
     }
 
   }, []);
 
+  console.log('user', user);
+  console.log('token', token);
+
   return (
     <div className="app">
       {
-        token ? (
-          <Player />
-        ) : (
+        token ? 
+          <Player spotify={spotify}/>
+         : 
           <Login />
-        )
       }
     </div>
   );
--- spotifyclone/src/Player.js
+++ spotifyclone/src/Player.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Player.css';
+import Sidebar from './Sidebar';
+import Body from './Body';
+import Footer from './Footer'
 
 function Player() {
     return (
-        <div>
-            <h1> welcome</h1>
+        <div className="player">
+            <div className="player-body">
+                <Sidebar />
+                <Body />
+            </div>
+           <Footer />
         </div>
     )
 }
