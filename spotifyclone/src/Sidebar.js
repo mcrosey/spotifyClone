a8cfe4fbee1ee58b6dec965fce890e679bf3d7b7
mcrosey <80073115+mcrosey@users.noreply.github.com>
2021-07-28 20:14:14 -0400

    footer complete

--- spotifyclone/src/Sidebar.js
+++ spotifyclone/src/Sidebar.js
@@ -4,8 +4,11 @@ import SidebarOption from './SidebarOption';
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import { useDataLayerValue } from './DataLayer';
 
 function Sidebar() {
+    const [{ playlists }, dispatch] = useDataLayerValue();
+
     return (
         <div className="sidebar">
             <img className="sidebar-logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
@@ -17,8 +20,9 @@ function Sidebar() {
             <strong className="sidebar-title">PLAYLIST</strong>
             <hr />
 
-            <SidebarOption title="Hip hop" />
-            <SidebarOption title="Countery" />
+            {playlists?.items?.map((playlist) => (
+                <SidebarOption title={playlist.name} />
+            ))}
         </div>
     )
 }
